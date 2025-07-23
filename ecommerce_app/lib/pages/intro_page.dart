11bc382bdb5e54f2a9825de22f4d44220db07fe6
Dario Orlovich <orlodax@gmail.com>
2025-07-23 18:21:03 +0100

    ecommerce app

---
+++ ecommerce_app/lib/pages/intro_page.dart
@@ -0,0 +1,68 @@
+import 'package:ecommerce_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/logo.png', height: 240),
+              ),
+              const SizedBox(height: 24),
+
+              // title
+              const Text(
+                'SHOES!',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+              ),
+
+              // subtitle
+              const Text(
+                'What are millions, when they give you shoes in exchange?',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 24),
+
+              // start now button
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
