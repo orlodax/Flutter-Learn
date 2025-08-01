084e80c6f7225c1b653e083f79543f8ab77b6157
Dario Orlovich <orlodax@gmail.com>
2025-08-01 16:33:01 +0100

    login app - firebase setup

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
